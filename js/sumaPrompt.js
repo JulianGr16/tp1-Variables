1397187fe34f883d63f51de70e3ebf9706254305
JulianGaray16 <juliangaray.2116@gmail.com>
2024-07-04 14:02:35 -0300

    terminamos el ejercicio 5

---
+++ js/sumaPrompt.js
@@ -0,0 +1,5 @@
+let num1 = prompt("ingrese el primer valor");
+let num2 = prompt("ingrese el segundo valor");
+let resultado = num1 + num2;
+
+document.write("el resultado de la suma es: " + resultado)
