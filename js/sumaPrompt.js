02cc67760825f604f4573b8a9f07449e6e33040a
JulianGaray16 <juliangaray.2116@gmail.com>
2024-07-04 16:58:57 -0300

    se realizaron correcciones en el ingreso de valores

--- js/sumaPrompt.js
+++ js/sumaPrompt.js
@@ -1,5 +1,5 @@
-let num1 = prompt("ingrese el primer valor");
-let num2 = prompt("ingrese el segundo valor");
+let num1 = parseInt(prompt("ingrese el primer valor"));
+let num2 = parseInt(prompt("ingrese el segundo valor"));
 let resultado = num1 + num2;
 
-document.write("el resultado de la suma es: " + resultado)
+document.write(`El resultado de la suma es: ${resultado}`);
