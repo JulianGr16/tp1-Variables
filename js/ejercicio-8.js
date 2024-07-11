509c870eaa9fbd0091758759fe595591c73580df
JulianGaray16 <juliangaray.2116@gmail.com>
2024-07-11 00:51:19 -0300

    se realizo correctamente el ejercicio 8

---
+++ js/ejercicio-8.js
@@ -0,0 +1,12 @@
+let numero = parseInt(prompt("ingrese un numero"));
+
+if(numero === 2 || numero === 4 || numero === 6 ||
+    numero === 8 || numero === 10 || numero === 12 ||
+    numero === 14 || numero === 16 || numero === 18 ||
+    numero === 20
+){
+    document.write("el numero si es divisible por 2");
+}
+else{
+    document.write("este numero no es divisible por 2");
+}
